c6c5bef2005bb5bee8a3052535dad99174338b37
gaurang1123 <gautamkhoraba1@gmail.com>
2025-03-03 14:42:28 +0530

    updated css

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
+// SMOOTH SCROLL NAV LINKS
+
 document.querySelectorAll('.nav-links a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault(); // Prevent default jump behavior
+        
+        const targetId = this.getAttribute('href').substring(1); // Get target section ID
+        const targetElement = document.getElementById(targetId);
+        if (!targetElement) return; // If the section doesn't exist, do nothing
+
+        const headerOffset = 50; // Adjust this value based on your navbar height
+        const elementPosition = targetElement.getBoundingClientRect().top + window.scrollY;
+        const offsetPosition = elementPosition - headerOffset;
+        
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth' // Enables smooth scrolling effect
+        });
+    });
+});
+
+// SMOOTH SCROLL MOBILE NAV LINKS
+
+document.querySelectorAll('.small-screen-nav-link  a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault(); // Prevent default jump behavior
 
@@ -17,6 +40,9 @@ document.querySelectorAll('.nav-links a[href^="#"]').forEach(anchor => {
     });
 });
 
+// HOME TEXT 
+
+
 
 
 /*===== SCROLL REVEAL ANIMATION =====*/
@@ -25,7 +51,6 @@ const sr = ScrollReveal({
     distance: '60px',
     duration: 2000,
     delay: 200,
-//     reset: true
 });
 
 sr.reveal('.home-text, .about-img, .skills-items, .skills-logo',{}); 
@@ -33,6 +58,8 @@ sr.reveal('.profile-img, .about-text, .skills__img',{delay: 400});
 sr.reveal('.home-links',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .contact-input',{interval: 200}); 
 
+
+//  DYNAMIC NAVIGATION
 
 const ham = document.querySelector(".nav-ham");
 const hamclose = document.querySelector(".fa-xmark");
